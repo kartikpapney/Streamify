5749d6e09eb628e96759c250bb51dd72cf906161
Kartik Papney <kartik.papney@wobot.ai>
2025-01-05 01:18:30 +0530

    update: uploader host

--- uploader/main.js
+++ uploader/main.js
@@ -22,7 +22,7 @@ async function getVideoDetails(link, tags) {
         const $ = cheerio.load(response.data);
         const title = $("meta[name='title']").attr("content");
 
-        const data = await axios.post("http://localhost:3001/api/movies/", { thumbnail: thumbnail, title: title, link: link, tags: tags}, {headers: {Authorization: `Bearer ${process.env.PLAYLIST_TOKEN}`}})
+        const data = await axios.post(`${process.env.BE_HOST}/api/movies/`, { thumbnail: thumbnail, title: title, link: link, tags: tags}, {headers: {Authorization: `Bearer ${process.env.PLAYLIST_TOKEN}`}})
 
     } catch (error) {
         console.error('Error fetching video details:', error);
